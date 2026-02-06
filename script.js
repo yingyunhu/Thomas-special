117cd1ec7c16f3f3a28f62c1c6232ecac6e10d6e
yingyunhu <112943865+yingyunhu@users.noreply.github.com>
2026-02-06 23:20:18 +0100

    script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ bgSong.loop = true;
 bgSong.volume = 0.5;
 
 // ✅ Kahoot lobby music (starts when letter opens, loops)
-const kahootSong = new Audio("https://files.catbox.moe/czlyyt.mp3");
+const kahootSong = new Audio("https://files.catbox.moe/rudqvb.mp3"); // temporary placeholder
 kahootSong.loop = true;
 kahootSong.volume = 0.4;
 
